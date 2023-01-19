3c4e8cb2f1512379974b92459a3290a7766f277e
AoThen <32790381+AoThen@users.noreply.github.com>
2023-01-19 21:07:11 +0800

    新增默认和VMware测试

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Timezone
+sed -i '/timezone='\''UTC'\''/a\\t\tset system.@system[-1].zonename='\''Asia/Shanghai'\''' package/base-files/files/bin/config_generate
 
 # sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
