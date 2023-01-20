79b485e17e9b06ed8d1b8680383ca86266b5b182
AoThen <32790381+AoThen@users.noreply.github.com>
2023-01-20 21:44:00 +0800

    argon

--- diy-part2_lede.sh
+++ diy-part2_lede.sh
@@ -18,10 +18,20 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #修改主机名
 # sed -i 's/OpenWrt/Xiaomi-AX6S/g' package/base-files/files/bin/config_generate
 
-rm -r package/lean/luci-theme-argon
+# rm -r package/lean/luci-theme-argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #添加额外软件包
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-# git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
+
+#取消原主题luci-theme-bootstrap为默认主题
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+#sed -i 's/luci-theme-bootstrap/luci-theme-ifit/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+# 修改默认wifi名称ssid为Xiaomi_R4A
+#sed -i 's/ssid=OpenWrt/ssid=openwrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
