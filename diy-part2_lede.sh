19dd31f170e1b19822aafa24829ee037e90d7c94
AoThen <32790381+AoThen@users.noreply.github.com>
2023-01-23 12:12:13 +0800

    argon替换尝试

--- diy-part2_lede.sh
+++ diy-part2_lede.sh
@@ -19,7 +19,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/OpenWrt/Xiaomi-AX6S/g' package/base-files/files/bin/config_generate
 
 # rm -r package/lean/luci-theme-argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #添加额外软件包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
