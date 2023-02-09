350605446fd05ce5cba2a9dfa326cbcfe8f054b8
AutoX <32790381+AoThen@users.noreply.github.com>
2023-02-09 23:01:07 +0800

    修改argon主题

--- diy-part2_lede.sh
+++ diy-part2_lede.sh
@@ -13,25 +13,28 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+# Clear the login password
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-#修改主机名
-# sed -i 's/OpenWrt/Xiaomi-AX6S/g' package/base-files/files/bin/config_generate
+# 修改版本为编译日期
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by AoThen/g" package/lean/default-settings/files/zzz-default-settings
+
+#替换aliyundrive-webdav高版本
+sed -i 's/1.10.3/1.10.6/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
+
 
 #删除原默认主题
-# rm -r package/lean/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 
-#取消原主题luci-theme-bootstrap为默认主题
-# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 #设置luci-theme-argon默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
 
-#替换aliyundrive-webdav高版本
-sed -i 's/1.10.3/1.10.6/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
-
 #添加额外软件包
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06 #Lean源码
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config #加入JerryKuKu’s Argon Config
